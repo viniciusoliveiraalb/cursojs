75d3ea2597fca959ba9abce93259dedf46ec5f6d
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-01-13 02:23:17 -0400

    Aula 11 - Condições
    Aprendendo mais sobre as condições simples e compostas.

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 8
+console.log(`A velocidade do seu caro é ${vel} Km/h`)
+if(vel > 60){ // condição  simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o sinto de segurança!')
