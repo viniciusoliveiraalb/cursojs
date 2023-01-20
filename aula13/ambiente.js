b5313a4d339edf186d12d9f547326e2cc642f702
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-01-19 20:30:53 -0400

    Estruturas de Repetições
    while e do...while

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6) // TESTE LÓGICO NO FINAL
+
+
+/* 
+
+TESTE LÓGICO NO INICIO
+======================
+
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
