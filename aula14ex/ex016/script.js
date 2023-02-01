ca181e6f4be4716b5d5122d6110e71bcabc3ad4c
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-02-01 04:59:16 -0400

    Resolvendo o exercicio 'CONTADOR'

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,28 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp') 
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0){
+        res.innerHTML = 'Impossivel contar!'
+    }else{
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            // CONTAGEM CRESCENTE
+            for(let c = i;c <= f;c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else{
+            // CONTAGEM DECRESCENTE
+            for(let c = i;c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+}
---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,6 @@
+var n = 5
+var prod = 1
+while(prod <= 10){
+    console.log(`${n} X ${prod} : `+ (prod*n))
+     prod++
+}
