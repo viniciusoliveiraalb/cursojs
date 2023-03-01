deb04adbde0abefbe010589588129314fe99cd15
Vinicius Oliveira <75396887+viniciusoliveiraalb@users.noreply.github.com>
2023-03-01 02:04:25 -0400

    erro gramaticalg

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -3,4 +3,4 @@ console.log(`A velocidade do seu caro é ${vel} Km/h`)
 if(vel > 60){ // condição  simples
     console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
 }
-console.log('Dirija sempre usando o sinto de segurança!')
+console.log('Dirija sempre usando o cinto de segurança!')
